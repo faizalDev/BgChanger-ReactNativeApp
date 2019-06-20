329a9eb9be448ef633a68f103123c05abe2e8700
FaizalAndro <37078576+FaizalAndro@users.noreply.github.com>
2019-06-20 05:57:35 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+export default class App extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {randomColor:null};
+  }
+    
+  
+
+  getRandomColor = () =>{
+    return (
+      "rgb("+
+      Math.floor(Math.random()*256)+
+      ","+
+      Math.floor(Math.random()*256)+
+      ","+
+      Math.floor(Math.random()*256)+")"
+    )
+  }
+  myButtonPressed = () => {
+    this.setState({randomColor: this.getRandomColor()});
+    
+  }
+
+  render() {
+    return (
+
+      <View style={[styles.container, {backgroundColor:this.state.randomColor}]}>
+        <TouchableOpacity onPress={this.myButtonPressed}>
+          <Text style={styles.tetxstyle}>Click BackGround</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  tetxstyle: {
+    fontSize: 30,
+    backgroundColor: "#E71C23",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    color: "#fff",
+    borderRadius: 10,
+    borderWidth:2,
+    borderColor:'#fff',
+  }
+});
